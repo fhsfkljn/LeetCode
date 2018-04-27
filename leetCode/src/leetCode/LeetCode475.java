9b22f36ebdcfc913642b50f5e558141db49e5f6b
fhsfkljn <895910258@qq.com>
2018-04-27 22:23:24 +0800

    leetCode475

---
+++ leetCode/src/leetCode/LeetCode475.java
@@ -0,0 +1,39 @@
+package leetCode;
+
+import java.util.Arrays;
+
+//475. Heaters
+/**
+ * Winter is coming! Your first job during the contest is to design a standard
+ * heater with fixed warm radius to warm all the houses.
+ * 
+ * Now, you are given positions of houses and heaters on a horizontal line, find
+ * out minimum radius of heaters so that all houses could be covered by those
+ * heaters.
+ * 
+ * So, your input will be the positions of houses and heaters seperately, and
+ * your expected output will be the minimum radius standard of heaters.
+ * 
+ * @author 89591
+ *
+ */
+public class LeetCode475 {
+	
+	public int findRadius(int[] houses, int[] heaters) {
+		Arrays.sort(heaters);
+        int result = Integer.MIN_VALUE;
+        
+        for (int house : houses) {
+            int index = Arrays.binarySearch(heaters, house);
+            if (index < 0) {
+        	index = -(index + 1);
+            }
+            int dist1 = index - 1 >= 0 ? house - heaters[index - 1] : Integer.MAX_VALUE;
+            int dist2 = index < heaters.length ? heaters[index] - house : Integer.MAX_VALUE;
+        
+            result = Math.max(result, Math.min(dist1, dist2));
+        }
+        
+        return result;
+	}
+}

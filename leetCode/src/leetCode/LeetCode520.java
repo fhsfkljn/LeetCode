e29654badc23411ad00ca297929256b75c501b34
fhsfkljn <895910258@qq.com>
2018-05-07 21:10:50 +0800

    leetCode520

---
+++ leetCode/src/leetCode/LeetCode520.java
@@ -0,0 +1,24 @@
+package leetCode;
+
+//520. Detect Capital
+/**
+ * 给定一个单词，你需要判断单词的大写使用是否正确。
+ * 
+ * 我们定义，在以下情况时，单词的大写用法是正确的：
+ * 
+ * 全部字母都是大写，比如"USA"。 单词中所有字母都不是大写，比如"leetcode"。 如果单词不只含有一个字母，只有首字母大写， 比如
+ * "Google"。 否则，我们定义这个单词没有正确使用大写字母。
+ * 
+ * @author 89591
+ *
+ */
+public class LeetCode520 {
+	
+	public static boolean detectCapitalUse(String word) {
+		return word.matches("([A-Z][a-z]*)|([a-z]+)|([A-Z]+)");
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(detectCapitalUse("usa"));
+	}
+}
